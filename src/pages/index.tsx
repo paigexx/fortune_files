776866d19e3b14d70146504a8a258a913508808c
paigexx <pcjones12@gmail.com>
2024-10-10 09:48:37 -0500

    feat: updates button disable styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,8 +78,8 @@ export default function Home() {
           />
           <button
             type="submit"
-            disabled={uploading || !file}
-            className="px-4 py-2 bg-lime-500 text-black rounded-full border border-black hover:bg-lime-500 hover:text-white transition duration-300"
+            disabled={!file}
+            className="px-4 py-2 bg-lime-500 text-black rounded-full border border-black hover:bg-lime-500 hover:text-white transition duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
           >
             Get Fortune 🥠
           </button>
